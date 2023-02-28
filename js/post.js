ed7db60861d47f8be602b31d74a09def637a328c
islamajabul <ajabulmd1@gmail.com>
2023-02-28 14:26:42 +0800

    update

--- js/post.js
+++ js/post.js
@@ -1,12 +1,12 @@
-function loadPosts(){
+function loadPosts() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(res => res.json())
-    .then(data => displayPost(data))
+        .then(res => res.json())
+        .then(data => displayPost(data))
 }
 
-function displayPost(posts){
+function displayPost(posts) {
     const postsContainer = document.getElementById('posts-container');
-    for(const post of posts){
+    for (const post of posts) {
         const postDiv = document.createElement('div');
         postDiv.classList.add('post')
         console.log(post);
